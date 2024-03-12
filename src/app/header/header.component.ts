2c9f33df115dc483f66f2df09c556d5650b143c5
demarcoeduard <edydumy@gmail.com>
2024-03-12 23:52:09 +0200

    edited header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  imagePath = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      if (this.router.url === '/home') {
+        this.imagePath = 'assets/logo-active.png';
+      } else {
+        this.imagePath = 'assets/logo.png';
+      }
+    })
+  }
 
 }
