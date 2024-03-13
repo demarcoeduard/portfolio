d2e71ec062715db6db74670a442c54252863505a
demarcoeduard <edydumy@gmail.com>
2024-03-14 00:44:42 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutMeComponent } from './about-me/about-me.component';
-import { ContactMeComponent } from './contact-me/contact-me.component';
+import { AboutMeComponent } from './about/about-me.component';
+import { ContactMeComponent } from './contact/contact-me.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'about-me', component: AboutMeComponent},
-  {path: 'contact-me', component: ContactMeComponent}
+  {path: 'about', component: AboutMeComponent},
+  {path: 'contact', component: ContactMeComponent}
 ];
 
 @NgModule({
