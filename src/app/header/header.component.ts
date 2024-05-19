29b7cb5dffd4d32f46949b9fb73ebb70f492f5a6
demarcoeduard <edydumy@gmail.com>
2024-05-19 22:14:00 +0300

    finalized header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ transform: 'scaleX(0)' }),
+        animate('.25s ease', style({ transform: 'scaleX(1)' }))
+      ]),
+      transition(':leave', [
+        style({ transform: 'scaleX(1)' }),
+        animate('.25s ease', style({ transform: 'scaleX(0)' }))
+      ])
+    ])
+  ]
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  @ViewChild('menu') menu!: ElementRef;
+  isOpen = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.isOpen = false;
+      this.menu.nativeElement.setAttribute('aria-expanded', false);
+    })
+  }
+
+  @HostListener('window:resize') updateNavState() {
+    if (window.innerWidth >= 1240) {
+      this.isOpen = false;
+      this.menu.nativeElement.setAttribute('aria-expanded', false);
+    }
+  }
+
+  onOpen() {
+    this.isOpen = !this.isOpen;
+    this.menu.nativeElement.setAttribute('aria-expanded', this.isOpen);
+  }
 }
