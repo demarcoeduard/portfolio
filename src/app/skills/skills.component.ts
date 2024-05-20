27a67d22e0329fe5205f9ec1843bc44958a2846b
demarcoeduard <edydumy@gmail.com>
2024-05-20 23:04:19 +0300

    finalized skills component

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./skills.component.css']
 })
 export class SkillsComponent {
-
+  skills = [
+    {
+      logo: '../../assets/html-logo.png',
+      text: 'HTML'
+    },
+    {
+      logo: '../../assets/css-logo.png',
+      text: 'CSS'
+    },
+    {
+      logo: '../../assets/javascript-logo.png',
+      text: 'JavaScript'
+    },
+    {
+      logo: '../../assets/typescript-logo.png',
+      text: 'TypeScript'
+    },
+    {
+      logo: '../../assets/angular-logo.png',
+      text: 'Angular'
+    },
+    {
+      logo: '../../assets/git-logo.png',
+      text: 'Git'
+    },
+    {
+      logo: '../../assets/figma-logo.png',
+      text: 'Figma'
+    },
+    {
+      logo: '../../assets/sass-logo.png',
+      text: 'Sass'
+    },
+    {
+      logo: '../../assets/bootstrap-logo.png',
+      text: 'Bootstrap'
+    },
+    {
+      logo: '../../assets/tailwind-logo.png',
+      text: 'Tailwind'
+    }
+  ]
 }
