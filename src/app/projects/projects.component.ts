4a078a695f53e3c8fb832dad0486ce71df8b40f1
demarcoeduard <edydumy@gmail.com>
2024-05-21 09:52:47 +0300

    finalized projects component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent {
-
+  projects = [
+    {
+      img: '../../assets/future-foundry.png',
+      title: 'Future Foundry',
+      link: 'https://demarcoeduard.github.io/future-foundry/',
+      skills: ['Angular', 'TypeScript', 'HTML', 'CSS'],
+      description: 'Connects businesses with vetted developers through a user-friendly website. It has straightforward forms that simplify communication, while clear design and responsive layout ensure a smooth experience on all devices.'
+    },
+    {
+      img: '../../assets/hospital-crm.png',
+      title: 'Hospital CRM',
+      link: 'https://demarcoeduard.github.io/hospital-crm/',
+      skills: ['Angular', 'TypeScript', 'HTML', 'CSS'],
+      description: 'Organizes doctor, patient, and appointment data in one central platform, improving hospital operations. Its intuitive interface allows adding, editing, and deleting info with ease. The real-time updates ensure data accuracy, while responsive design optimizes access on all devices.'
+    }
+  ]
 }
